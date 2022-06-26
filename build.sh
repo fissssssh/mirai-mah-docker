72790bca2edb821ceee6437738e9719450de4c00
fish <zhangjiancheng2021@outlook.com>
2022-06-26 18:32:49 +0800

    fix build.sh
    Signed-off-by: fish <zhangjiancheng2021@outlook.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 TAG=v0.0.1
 
-mkdir mcl
-cd mcl
 latest_download_url=""$(curl -s https://api.github.com/repos/iTXTech/mirai-console-loader/releases/latest | grep browser_download_url | cut -f4 -d "\"")""
 wget -O mcl.zip $latest_download_url
+mkdir mcl
 unzip -d mcl mcl.zip
 docker build -t mirai-mah-docker:$TAG .
 docker push fissssssh/mirai-mah-docker:$TAG
